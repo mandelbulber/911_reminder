2c74a80ae764b1629bec4e6df53c9e490be18479
unknown <andrin@eugsters.ch>
2023-11-16 17:53:33 +0100

    Adapt for production

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,10 @@
-# bot.py
 import datetime
 from time import sleep
 import discord
 
-DISCORD_TOKEN = 'MTE3MzU2MDg2NTE4MjcxNTk1NQ.GLZCfu.wlDKpTpF-ZFUKFN_hYxm49sQgXdrLvEm7dLaek' # don't tell me nothing bout security god dammit
-DISCORD_GUILD = 'Server von Horst Konrader'
-DISCORD_CHANNEL = 1173563257466920963
+DISCORD_TOKEN = 'MTE3NDcyMDE4MDA0ODM3NTg0OA.GP-HTe.PcijwPixLfj5mTsANN-v_KrWicP_Ngq8mLuh8I' # don't tell me nothing bout security god dammit
+DISCORD_GUILD = 'Konis Discord'
+DISCORD_CHANNEL = 982685695233630299
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -22,10 +21,10 @@ async def on_ready():
         current_time = datetime.datetime.now()
         if current_time.hour == 9 and current_time.minute == 11:
             channel = client.get_channel(DISCORD_CHANNEL)
-            with open('src\911.gif', 'rb') as f:
+            with open('/home/dev/911_reminder/911.gif', 'rb') as f:
                 gif = discord.File(f)
                 await channel.send(file=gif)
                 print(f'message sent')
-            sleep(5)
+            sleep(500)
 
 client.run(DISCORD_TOKEN)
