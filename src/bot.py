6caa1e6ca823162e832a4097fb0c0dc3d9220457
Eugstea1 <andrin.eugster@roche.com>
2023-11-16 16:33:00 +0100

    Finalize implementation

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,11 @@
 # bot.py
-from asyncio import sleep
 import datetime
+from time import sleep
 import discord
 
 DISCORD_TOKEN = 'MTE3MzU2MDg2NTE4MjcxNTk1NQ.GLZCfu.wlDKpTpF-ZFUKFN_hYxm49sQgXdrLvEm7dLaek' # don't tell me nothing bout security god dammit
 DISCORD_GUILD = 'Server von Horst Konrader'
+DISCORD_CHANNEL = 1173563257466920963
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -19,12 +20,12 @@ async def on_ready():
 
     while(1):
         current_time = datetime.datetime.now()
-        if current_time.second == 10:
-        #if current_time.hour == 9 and current_time.minute == 11:
-            # post a message in a channel
-            channel = client.get_channel(1173563257466920963)
-            await channel.send('9.11')
+        if current_time.hour == 9 and current_time.minute == 11:
+            channel = client.get_channel(DISCORD_CHANNEL)
+            with open('src\911.gif', 'rb') as f:
+                gif = discord.File(f)
+                await channel.send(file=gif)
+                print(f'message sent')
             sleep(5)
-            print(f'message sent')
 
 client.run(DISCORD_TOKEN)
